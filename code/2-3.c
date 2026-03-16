4605ca538db48af0e5a559c2910a1cb815a36dbe
wyhoaule <153972304+wyhoaule@users.noreply.github.com>
2026-03-16 16:58:49 +0900

    2-3.c
    Print the individual bytes of an unsigned int.

--- code/2-3.c
+++ code/2-3.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+   unsigned int x = 0x12345678;
 
-    return 0;
+  unsigned char* addr = (unsigned char*)&x;
+
+ printf("%x\n", *addr);
+printf("%x\n", *(addr + 1));
+printf("%x\n", *(addr + 2));
+printf("%x\n", *(addr + 3));
+
+return 0;
 }
-
