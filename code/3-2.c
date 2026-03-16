5e0eaddc3b350c67329d398928cb245e2d743335
wyhoaule <153972304+wyhoaule@users.noreply.github.com>
2026-03-16 17:32:02 +0900

    3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int a = -1;
+    unsigned int b =1;
+
+    if (a < b) {
+        printf("a < b\n");
+    } else {
+        printf("a >= b ???\n");
+    }
 
     return 0;
 }
-
