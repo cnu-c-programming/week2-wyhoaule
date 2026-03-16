d5ce2da2cb0760f7a5f9daf8440eb41ae6ce16f0
wyhoaule <153972304+wyhoaule@users.noreply.github.com>
2026-03-16 16:45:31 +0900

    2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    
+	printf("char: %ld\n", sizeof(char));
+	printf("short: %ld\n", sizeof(short));
+	printf("int: %ld\n", sizeof(int));
+	printf("unsigned int: %ld\n", sizeof(unsigned int));
+	printf("long: %ld\n", sizeof(long));
+	printf("long long: %ld\n", sizeof(long long));
+	printf("float: %ld\n", sizeof(float));
 
-    return 0;
+	return 0;
 }
