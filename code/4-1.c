37a6c63b711741d0dd009312e9d3a5cc359db1e6
wyhoaule <153972304+wyhoaule@users.noreply.github.com>
2026-03-17 14:21:53 +0900

    4-1.ㅊ

--- code/4-1.c
+++ code/4-1.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int a, b;
+    char op;
+
+    scanf("%d %d %c", &a, &b, &op);
+    
+    if (op == '+') {
+	printf("%d", a+b);
+    } else if (op == '-') {
+        printf("%d", a-b);
+    } else if (op == '*') {
+        printf("%d", a*b);
+    } else if (op == '/') {
+        printf("%d", a/b);
+    }
 
     return 0;
 }
-
