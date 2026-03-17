43483b6761d5edc3a09640e37fefc83ee4fb9239
wyhoaule <153972304+wyhoaule@users.noreply.github.com>
2026-03-17 15:43:20 +0900

    4-2.ㅊ

--- code/4-2.c
+++ code/4-2.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main()
-{
-
-    return 0;
+int main() {
+    for(int i = 1; i<=9; i++) {
+        for(int j = 1; j<=9; j++) {
+            printf("%d * %d = %d\n", i, j, i*j);
+        }
+    }
 }
-
