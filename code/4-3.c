b0e451e1ce70de1ae4e06a1bc8ce2f90edc9e588
wyhoaule <153972304+wyhoaule@users.noreply.github.com>
2026-03-19 12:39:19 +0900

    4-3.c
    Added prime number check and user input handling.

--- code/4-3.c
+++ code/4-3.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main()
-{
+int main() {
+    int a;
+    bool boolean = true;
+    scanf("%d", &a);
 
-    return 0;
+    if (a < 2) boolean = false;
+
+    for(int i = 2; i <= a - 1; i++) {
+        if(a % i == 0){
+            boolean = false;
+            break;
+        }
+    }
+
+    if(boolean == true) {
+        printf("true");
+    } else {
+        printf("false");
+    }
 }
-
